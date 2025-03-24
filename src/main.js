505ba38e99658a57f4c8dd5514eff563f16078ee
Paulo Montefusco <paulomontefusco.junior@gmail.com>
2025-03-24 16:22:40 -0300

    Importing pinia

--- src/main.js
+++ src/main.js
@@ -2,8 +2,12 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
+import { createPinia } from 'pinia';
 import VueTheMask from 'vue-the-mask'
 
-const app = createApp(App)
-app.use(VueTheMask)
-app.mount('#app')
+const app = createApp(App);
+const pinia = createPinia();
+
+app.use(VueTheMask);
+app.use(pinia);
+app.mount('#app');
