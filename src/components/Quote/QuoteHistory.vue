101c3e230ff3d07aae9c69908b982e29f152e8b4
Paulo Montefusco <paulomontefusco.junior@gmail.com>
2025-03-24 14:01:24 -0300

    Adding quote components inside the Quote folder

---
+++ src/components/Quote/QuoteHistory.vue
@@ -0,0 +1,69 @@
+<template>
+    <div class="quoteHistory" v-if="history.length > 0">
+        <h2>Histórico de Cotações</h2>
+        <ul>
+            <li v-for="cotacao in history" :key="cotacao.timestamp" class="quoteItem">
+                <QuoteInfo
+                    :cepOrigem="cotacao.dados.cep_origin"
+                    :cepDestino="cotacao.dados.cep_destination"
+                    :serviceDescription="cotacao.resultados[0].ServiceDescription"
+                    :shippingPrice="cotacao.resultados[0].ShippingPrice"
+                />
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+    import QuoteInfo from './QuoteInfo.vue';
+    
+    export default {
+        components: {
+            QuoteInfo
+        },
+
+        props: {
+            history: {
+                type: Array,
+                required: true
+            }
+        },
+    }
+</script>
+
+<style scoped>
+    .quoteHistory {
+        background-color: var(--white);
+        border: 1px solid var(--border-color);
+        border-radius: .5rem;
+        box-shadow: 0 .5rem 1.25rem 0 rgba(183, 198, 206, 0.30);
+
+        margin-top: 2rem;
+        padding: 0 2rem;
+        width: 100%;
+        height: 20rem;
+        overflow: auto;
+    }
+
+    h2 {
+        position: sticky;
+        top: 0;
+
+        background-color: var(--white);
+        color: var(--black);
+        
+        font-size: 1rem;
+        font-weight: 500;
+
+        letter-spacing: .6px;
+        padding: 1.5rem 0 .5rem 0;
+    }
+    
+    .quoteItem {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        padding: 1rem 0;
+        border-bottom: 1px solid #eee;
+    }
+</style>
---
+++ src/components/Quote/QuoteInfo.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="quoteInfo">
+        <p><strong>Origem</strong>: {{ cepOrigem }}</p>
+        <p><strong>Destino</strong>: {{ cepDestino }}</p>
+        <p><strong>Resultados</strong>: {{ serviceDescription }} - R$ {{ formattedPrice }}</p>
+    </div>
+</template>
+
+<script>
+    import { ref } from 'vue';
+    import { useCurrencyFormat } from '@/hooks/useCurrencyFormat';
+
+    export default {
+        props: {
+            cepOrigem: {
+                type: String,
+                required: true
+            },
+            cepDestino: {
+                type: String,
+                required: true
+            },
+            serviceDescription: {
+                type: String,
+                required: true
+            },
+            shippingPrice: {
+                type: Number,
+                required: true
+            }
+        },
+
+        setup(props) {
+            const { formattedPrice } = useCurrencyFormat(ref(props.shippingPrice));
+
+            return {
+                formattedPrice,
+            };
+        },
+    }
+</script>
+
+<style scoped>
+    .quoteInfo {
+        flex-grow: 1;
+    }
+</style>
