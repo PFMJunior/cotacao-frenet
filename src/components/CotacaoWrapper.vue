f8939d19faafa9b3cd11e74dc82b030b5f0f1726
Paulo Montefusco <paulomontefusco.junior@gmail.com>
2025-03-24 16:24:47 -0300

    Creating a parent component to share the same state

---
+++ src/components/CotacaoWrapper.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        <Formulario />
+        <CotacaoFrete />
+    </div>
+</template>
+
+<script setup>
+    import Formulario from './Formulario.vue';
+    import CotacaoFrete from './CotacaoFrete.vue';
+</script>
